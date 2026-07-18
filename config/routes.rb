b958562dc322abac2fc8fe485431f575cce64ef9
sakusaku1224 <sakusaku1224rako@gmail.com>
2026-07-18 04:53:30 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  # URLとアクション２つの意味を持つ
   get 'static_pages/home'
   get 'static_pages/help'
-  root "hello#index"
+  get 'static_pages/about'
 end
