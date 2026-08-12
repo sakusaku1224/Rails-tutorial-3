3466b8d0b2a0cdfcda5d735758c820008f35b83f
sakusaku1224 <sakusaku1224rako@gmail.com>
2026-08-12 12:05:27 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,40 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    # => app/views/users/show.html.erb
+  end
+
+  # GET /users/new
   def new
-    # => GET app/views/users/new.html.erb
+    @user = User.new
+    # => app/views/users/new.html.erb
+  end
+
+  # POSt /users/create
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to the Sumple App!'
+      # リダイレクトを実行するとGETリクエストを送る /users/:id（新規のリクエストを発行する）
+      # createでPOSTリクエストを送った処理の途中でGETリクエストを送る
+      redirect_to @user
+      # redirect_to user_path(@user) #GET /user/:id
+      # userのインスタンスを渡すと対応するidを引っ張ってくる
+    else
+      flash.now[:alrt] = '登録に失敗しました'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup information' do
+    # ユーザーが失敗するテスト
+    # サインアップページに行く
+    get signup_path
+    # 以下のコードを実行してもUser.countの数値が変わらない。（適切なデータが入力されていないと保存されない）
+    assert_no_difference 'User.count' do
+      # 直接postメソッドを送る。その時にparamsに何を渡すのか指定をする
+      post users_path, params: { user: { name: '',
+                                         email: 'user@invalid',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
+    end
+    # 422のエラーコードをブラウザに返す
+    assert_response :unprocessable_entity
+    assert_select 'title', 'Sign up | Ruby on Rails Tutorial Sample App'
+  end
+
+  test 'valid signup information' do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Example User',
+                                         email: 'user@example.com',
+                                         password: 'password',
+                                         password_confirmation: 'password' } }
+    end
+    follow_redirect!
+    assert_response :success
+    assert_select 'title', 'Example User | Ruby on Rails Tutorial Sample App'
+  end
+end
